79af17efb5da84fdcc5464066a5e1cafef3e8ad9
Landon <landonscribbles@gmail.com>
2017-02-19 11:54:12 -0800

    The Windows github client is not so good

---
+++ Assets/Placeholders/Scripts/Unused_HandleElevatorDoors.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleElevatorDoors : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] elevatorDoors;
+    private bool elevatorDoorsDown;
+
+	void Start () {
+        elevatorDoorsDown = false;
+	}
+	
+    void OnTriggerEnter(Collider target) {
+        if (target.tag == "Player") {
+            if (elevatorDoorsDown) {
+                for (int i = 0; i < elevatorDoors.Length; i++) {
+                    elevatorDoors[i].SetActive(true);
+                }
+                elevatorDoorsDown = false;
+            } 
+        } else {
+            for (int i = 0; i < elevatorDoors.Length; i++) {
+                elevatorDoors[i].SetActive(false);
+            }
+            elevatorDoorsDown = true;
+        }
+    }
+}
