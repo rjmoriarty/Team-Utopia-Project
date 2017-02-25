a68b3486c02dc3dc03293c865e33d4bf737ce42b
Tatiana Bernardo <tatiana.mobe@gmail.com>
2017-02-25 16:10:49 +0000

    Added some pictograms, holograms and sound. Added an asset for holograms

---
+++ Assets/Scripts/AnimateHologram.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateHologram : MonoBehaviour {
+
+    public int matIndex = 0;
+    public Vector2 AnimRate = new Vector2(1.0f, 0.0f);
+    public string textureName = "_MainTex";
+
+    Vector2 Offset = Vector2.zero;
+
+    private void LateUpdate()
+    {
+        Offset += (AnimRate * Time.deltaTime);
+
+        if (GetComponent<Renderer>().enabled)
+        {
+            GetComponent<Renderer>().materials[matIndex].SetTextureOffset(textureName, Offset);
+        }
+    }
+
+
+}
